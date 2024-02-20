903b6184ebe46cd954d24de77838ffaacf7784c2
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-02-20 17:25:32 +0100

    [NOD-706] fix: added tracing log and exception throwing

---
+++ src/main/java/it/gov/pagopa/nodoverifykotodatastore/exception/AppException.java
@@ -0,0 +1,8 @@
+package it.gov.pagopa.nodoverifykotodatastore.exception;
+
+public class AppException extends Exception {
+
+    public AppException(String message) {
+        super(message);
+    }
+}
